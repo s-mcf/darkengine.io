1571d527a9519d5c28c0e17162a79d417872eb5b
Simon McFarlane <simon@piston.pw>
2015-09-24 04:46:19 +0000

    Add music

--- data/www/index.php
+++ data/www/index.php
@@ -62,10 +62,21 @@ a{
 	color: #7B7CC8;
 	text-decoration: none;
 }
+button{
+	background-color: rgba(0,0,0,0.5);
+	border: 1px solid #ddd;
+	color: #ddd;
+	font-family: Terminus, Terminus-woff, monospace;
+}
 </style>
 </head>
 <body>
-<div id="h">darkengine</div>
+<div id="h">darkengine
+<audio id="kyoku">
+<source src="/static/music/aisatsana.mp3" />
+</audio>
+<button id="begin">Take me with you</button>
+</div>
 <div id="contact">
 <ul>
 <li><a href="mailto:simon@piston.pw">Email</a></li>
@@ -80,5 +91,15 @@ a{
 <li>asm</li>
 </ul>
 </div>
+<script type="text/javascript">
+var kyoku = document.getElementById('kyoku'),
+    begin = document.getElementById('begin');
+
+begin.onclick = function () {
+	if(kyoku.paused){
+		kyoku.play();
+	}
+};
+</script>
 </body>
 </html>
